e28dca91dea5e840d8730bb4b6f46560be3a7668
Samraj Moorjani <samrajmoorjani@gmail.com>
2019-04-15 11:51:46 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -8,6 +8,7 @@ import glob
 import numpy as np
 import cv2
 
+print("updated1")
 # Reading in data and assigning labels
 image_list = []
 class_list = []
@@ -28,7 +29,7 @@ class_list = np.array(class_list)
 # Splitting dataset
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(image_list, class_list, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(image_list, class_list, test_size=0.2, random_state=101)
 
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -36,18 +37,18 @@ from keras.layers import Conv2D, MaxPooling2D
 
 # CNN Structure
 model = Sequential()
-model.add(Conv2D(256, (3, 3), input_shape=(64, 64, 3)))
+model.add(Conv2D(128, (3, 3), input_shape=(64, 64, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(128, (3, 3)))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(32))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 
